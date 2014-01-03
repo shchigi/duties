cd220cf49caa1368b02425211a826282b7d14f96
Denis Shchigelsky <denis.shchigelsky@gmail.com>
2014-01-03 22:13:41 +0400

    Added Duty class

--- People/models.py
+++ People/models.py
@@ -2,6 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+
+class Duty:
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=256)
+
+    def __unicode__(self):
+        print "Duty " + self.name + ". ",
+        print self.description
+
+
 class Person:
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=30)
+    duties = models.ManyToManyField(Duty)
