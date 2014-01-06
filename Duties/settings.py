e3007f3262ad766dcdeae731b478193f051a74be
Denis Shchigelsky <denis.shchigelsky@gmail.com>
2014-01-06 21:30:58 +0400

    Added  __unicode__ methods to Person and Duty

--- Duties/settings.py
+++ Duties/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/rakot/PycharmProjects/Duties/sqliteDuties.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/rakot/PycharmProjects/duties/sqliteDuties.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -121,8 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'Places',
-    'geoposition',
+    'People',
 )
 
 # A sample logging configuration. The only tangible logging
--- People/models.py
+++ People/models.py
@@ -3,16 +3,20 @@ from django.db import models
 # Create your models here.
 
 
-class Duty:
+class Duty (models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=256)
 
     def __unicode__(self):
-        print "Duty " + self.name + ". ",
-        print self.description
+        return unicode(self.name + ". " + self.description)
 
 
-class Person:
+class Person (models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=30)
     duties = models.ManyToManyField(Duty)
+
+    def __unicode__(self):
+        head = unicode(self.first_name + " " + self.last_name + ". Duties:\n")
+        duties_string = reduce (lambda x, y: x.__unicode__() + "\n" + y.__unicode__(), self.duties.all())
+        return unicode (head + duties_string)
