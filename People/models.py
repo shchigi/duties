4f8d7cb90e5fbdcf21cdffa9147bcfcd5c5ac28c
Denis Shchigelsky <denis.shchigelsky@gmail.com>
2014-01-14 23:56:52 +0400

    Renamed Duty class to DutyType class
    Added Duty class into model

--- People/models.py
+++ People/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
 
-class Duty (models.Model):
+class DutyType (models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=256)
 
@@ -14,9 +15,24 @@ class Duty (models.Model):
 class Person (models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=30)
-    duties = models.ManyToManyField(Duty)
+    duties = models.ManyToManyField(DutyType)
 
     def __unicode__(self):
         head = unicode(self.first_name + " " + self.last_name + ". Duties:\n")
         duties_string = reduce(lambda x, y: x.__unicode__() + "\n" + y.__unicode__(), self.duties.all())
         return unicode(head + duties_string)
+
+class Duty (models.Model):
+    person = models.ForeignKey(Person)
+    duty_type = models.ForeignKey(DutyType)
+    date = models.DateTimeField()
+
+    def __unicode__(self):
+        return unicode(self.date) + ": " + self.person.__unicode__() + ", " + self.duty_type.name
+
+    def __init__(self, person, duty_type, date = datetime.now()):
+        super(Duty, self).__init__()
+        self.date = date
+        self.duty_type = duty_type
+        self.person = person
+        print self
