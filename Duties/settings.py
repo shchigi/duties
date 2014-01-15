c1937f186d64974c8348a12a136f01e95ef24562
Denis Shchigelsky <denis.shchigelsky@gmail.com>
2014-01-15 23:19:24 +0400

    fixed constructor issue with Duty

--- Duties/settings.py
+++ Duties/settings.py
@@ -46,7 +46,7 @@ USE_I18N = True
 USE_L10N = True
 
 # If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
+USE_TZ = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'People',
+    # 'south',
 )
 
 # A sample logging configuration. The only tangible logging
--- People/models.py
+++ People/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from datetime import datetime
-
 # Create your models here.
 
 
@@ -19,20 +17,18 @@ class Person (models.Model):
 
     def __unicode__(self):
         head = unicode(self.first_name + " " + self.last_name + ". Duties:\n")
-        duties_string = reduce(lambda x, y: x.__unicode__() + "\n" + y.__unicode__(), self.duties.all())
+        try:
+            duties_string = unicode(reduce(lambda x, y: x.__unicode__() + "\n" + y.__unicode__(), self.duties.all()))
+        except TypeError:   # no duties
+            duties_string = "No duties yet\n"
         return unicode(head + duties_string)
+
 
 class Duty (models.Model):
     person = models.ForeignKey(Person)
     duty_type = models.ForeignKey(DutyType)
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
-        return unicode(self.date) + ": " + self.person.__unicode__() + ", " + self.duty_type.name
-
-    def __init__(self, person, duty_type, date = datetime.now()):
-        super(Duty, self).__init__()
-        self.date = date
-        self.duty_type = duty_type
-        self.person = person
-        print self
+        return (unicode(self.date) + ": " + self.person.first_name + " " + self.person.last_name + ", " +
+               self.duty_type.name)
