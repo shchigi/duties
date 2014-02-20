19a0085e518971aed8198dcd381d1e6dec1787e5
Denis Shchigelsky <denis.shchigelsky@gmail.com>
2014-02-21 00:18:14 +0400

    Added person-duty view

--- People/views.py
+++ People/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 
 from django.http import HttpResponse
-from People.models import Person
+from People.models import Person, DutyType
 from django.template.loader import get_template
 from django.template import Context
 
@@ -16,5 +16,10 @@ def person(request, person_name):
 def personlist(request):
     t = get_template('person_list.html')
     person_list = Person.objects.all()
-    html = t.render(Context({'person_list':person_list}))
+    for person in person_list:
+        person.duty_list = person.duty_set.all()
+    
+    duty_types = DutyType.objects.all()
+
+    html = t.render(Context({'person_list':person_list, 'duty_types':duty_types}))
     return HttpResponse(html)
