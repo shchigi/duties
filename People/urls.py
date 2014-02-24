817a11f4ba51ab90d31644ec105c9b73573a1e69
Denis Shchigelsky <denis.shchigelsky@gmail.com>
2014-02-25 02:53:19 +0400

    Added index view and modified duty_type view. Reconfigured urls

--- People/urls.py
+++ People/urls.py
@@ -5,8 +5,9 @@ from People import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^persons/$', views.personlist),
-    url(r'^(?P<person_name>\w+)/$', views.person, name='person'),
+    url(r'^types/(?P<duty_type_name>\w+)/$', views.duty_type_info, name='types'),
+    url(r'^persons/$', views.duty_type_info),
+#    url(r'^(?P<person_name>\w+)/$', views.person, name='person'),
 
 )
 
--- People/views.py
+++ People/views.py
@@ -2,24 +2,36 @@
 
 from django.http import HttpResponse
 from People.models import Person, DutyType
-from django.template.loader import get_template
-from django.template import Context
-
+from django.shortcuts import render_to_response, get_object_or_404
 
 def index(request):
-    return HttpResponse("Hello, you are at duties.")
+    duty_types = DutyType.objects.all()
+    for duty_type in duty_types:
+        # duty_type.link = link_prefix + "" + duty_type.name
+        duty_type.link = duty_type.name
+        print duty_type.link
+    return render_to_response('index.html', {'duty_types': duty_types})
+
 
 def person(request, person_name):
     person = Person.objects.get(first_name=person_name)
     return HttpResponse("You are looking at person %s" % person.__unicode__())
 
-def personlist(request):
-    t = get_template('person_list.html')
-    person_list = Person.objects.all()
+
+def duty_type_info(request, duty_type_name):
+    duty_type = get_object_or_404(DutyType, name=duty_type_name)
+    person_list = Person.objects.filter(duties=duty_type)
     for person in person_list:
         person.duty_list = person.duty_set.all()
-    
-    duty_types = DutyType.objects.all()
+        person.is_champion = False
+        person.is_looser = False
+    champion = max(person_list, key=lambda x: len(x.duty_list))
+    champion.is_champion = True
+    looser = min(person_list, key=lambda x: len(x.duty_list))
+    looser.is_looser = True
 
-    html = t.render(Context({'person_list':person_list, 'duty_types':duty_types}))
-    return HttpResponse(html)
+    return render_to_response('person_list.html',
+                              {'person_list': person_list,
+                               'duty_type': duty_type,
+                               'champion': champion,
+                               'looser': looser})
